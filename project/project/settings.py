a0499e0116fd7a7674d56292a848dc82a486b925
Joy Sutrdhar <sutradharjoy2006@gmail.com>
2026-01-28 20:31:16 +0600

    food app installed

--- project/project/settings.py
+++ project/project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'food.apps.FoodConfig',
     'hotel.apps.HotelConfig',
     'django.contrib.admin',
     'django.contrib.auth',
