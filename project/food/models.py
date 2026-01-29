50f129607db45473cb892ee997044238dd3ef212
Joy Sutrdhar <sutradharjoy2006@gmail.com>
2026-01-29 15:49:50 +0600

    item model added to DB -in this data model we will add food items

--- project/food/models.py
+++ project/food/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class item(models.Model):
+    def __str__(self):
+        return self.food_name 
+    food_name= models.CharField(max_length=200)
+    food_description=models.CharField()
+    food_price= models.IntegerField()
+    food_image=models.CharField(default='https://toppng.com/uploads/preview/clipart-free-seaweed-clipart-draw-food-placeholder-11562968708qhzooxrjly.png')
