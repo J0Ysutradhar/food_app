222907589c4dc10293b365c40e1ffa928238fd8b
Joy Sutrdhar <sutradharjoy2006@gmail.com>
2026-02-04 20:29:28 +0600

    login functionality added

--- project/users/urls.py
+++ project/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 app_name='users'
 urlpatterns = [
 
 path('signup/', views.signup, name='signup'),
+path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login')
 
 ]
--- project/users/views.py
+++ project/users/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserSignupForm
+from django.contrib import messages
 # Create your views here.
 
 def signup(request):
-    form=UserCreationForm(request.POST or None)
+    form=UserSignupForm(request.POST or None)
     if request.method == "POST":
         if form.is_valid():
             form.save()
+            username=form.cleaned_data.get('username')
+            messages.success(request, f"{username}, your account created successfully")
             return redirect('hotel:homepage')
     context={
         'form':form,
