8df8281095218cbcdf2de2f40417a320d9eb06b4
Joy Sutrdhar <sutradharjoy2006@gmail.com>
2026-02-04 16:45:57 +0600

    Sigup functionality added

--- project/users/views.py
+++ project/users/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def signup(request):
-    return HttpResponse('Im sinuppage')
+    form=UserCreationForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('hotel:homepage')
+    context={
+        'form':form,
+    }
+
+    return render(request, 'users/signup.html', context)
